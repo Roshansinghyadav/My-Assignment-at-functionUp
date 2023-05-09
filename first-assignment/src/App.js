7e483419bfdb9f3922f65a0ea3398f02e44651b8
Roshan singh yadav <callmersy8031@gmail.com>
2023-05-09 20:39:52 +0530

    this ia app.js file

---
+++ first-assignment/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <h1>Benifits of usiing react...</h1>
+    <ol>
+      <li>Component-based architecture</li>
+      <li>Virtual DOM for efficient updates</li>
+      <li>Rich ecosystem and community</li>
+      <li>Cross-platform development</li>
+      <li>Strong community support</li>
+      <button>Get Started</button>
+    </ol>
+    
+    </>
+        
+  );
+}
+
+export default App;
